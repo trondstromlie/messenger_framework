2bdcafc1fafb940b7c4bff880d993d1f69cd492a
Trond Strom <mybestlabs@gmail.com>
2020-05-27 14:57:51 +0200

    later on

--- templates/sendGenericTemplate.js
+++ templates/sendGenericTemplate.js
@@ -1,6 +1,16 @@
 const request = require('request');
 module.exports = function sendGenericTemplate(recipientId, respBody) {
    console.log(respBody);
+
+  const id_fields = request({ url: "https://graph.facebook.com/v2.6/" + recipientId,
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN,
+          fields: "first_name"
+        },
+        method: "GET"
+      });
+  console.log(id_fields);    
+
+
    const message = [{"title":"hei på deg","subtitle":respBody["msg"]}];
          let messageData = {
          "attachment": {

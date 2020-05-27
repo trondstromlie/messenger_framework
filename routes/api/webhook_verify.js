b298ccb8de9d02d461d21d4629b34cb76606e610
Trond Strom <mybestlabs@gmail.com>
2020-05-27 10:31:14 +0200

    veryfy token

--- routes/api/webhook_verify.js
+++ routes/api/webhook_verify.js
@@ -14,9 +14,7 @@ const http = require('http');
 
 
   router.get('/hook', function(req, res) {
-    const queryObject = url.parse(req.url,true).query;
-    console.log(queryObject["hub.verify_token"]);
-    
+    const queryObject = url.parse(req.url,true).query;    
     if (queryObject["hub.verify_token"] === process.env.VERIFY_TOKEN){ //
        console.log('webhook verified');
        res.status(200).send(queryObject["hub.challenge"]);

7eedc20c4cc13d822ea42e1092abe3a43da8a118
Trond Strom <mybestlabs@gmail.com>
2020-05-28 16:08:14 +0200

    lks

--- processes/quickreply.js
+++ processes/quickreply.js
@@ -22,12 +22,12 @@ module.exports = async function quickreply(event) {
     if (!event.message.is_echo) {
       const message = event.message;
       const senderID = event.sender.id;
-      const quickreply = JSON.stringify(event.message.quick_reply)
+      const quickreply = JSON.stringify(event.message.quick_reply.payload)
       console.log(event.sender);
       console.log("Received message from senderId: " + senderID);
       console.log("Message is: " + JSON.stringify(message));
 
-    console.log({quick_reply:event.message.quick_reply[0]});
+    console.log({quickreply);
     if (message.quickreply) {
 
       senderAction(senderID);

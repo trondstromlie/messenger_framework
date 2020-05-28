c34fe9091532c6a586c65ecf8d202ce7e1ad72e4
Trond Strom <mybestlabs@gmail.com>
2020-05-28 15:03:26 +0200

    lks

--- routes/api/webhook_verify.js
+++ routes/api/webhook_verify.js
@@ -35,6 +35,7 @@ const http = require('http');
 
        /* Iterate over each entry, there can be multiple entries
        if callbacks are batched. */
+       console.log({webhook:{req.body}})
        req.body.entry.forEach(function(entry) {
        // Iterate over each messaging event
           entry.messaging.forEach(function(event) {

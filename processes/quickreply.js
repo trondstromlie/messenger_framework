f4371fef9ef8226b752380fbdc48fded54a1e5b2
Trond Strom <mybestlabs@gmail.com>
2020-05-28 14:46:55 +0200

    lks

---
+++ processes/quickreply.js
@@ -0,0 +1,41 @@
+const request = require('request');
+const senderAction = require('../templates/senderAction');
+const sendMessage = require('../templates/sendMessage');
+const sendGenericTemplate = require('../templates/sendGenericTemplate');
+const quickReplies = require('../templates/quickReplies');
+
+module.exports = async function quickreply(event) {
+
+
+    //use a get request to fetch name fields from graph
+    await request({ url: "https://graph.facebook.com/v2.6/" + event.sender.id,
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN,
+          fields: "first_name,last_name"
+
+        },
+        method: "GET"
+      },  ( error,res,body ) => {
+          let user_fields = JSON.parse(body);
+
+
+
+    if (!event.message.is_echo) {
+      const message = event.message;
+      const senderID = event.sender.id;
+      console.log(event.sender);
+      console.log("Received message from senderId: " + senderID);
+      console.log("Message is: " + JSON.stringify(message));
+
+    if (message.text) {
+
+      senderAction(senderID);
+       sendMessage(senderID, {text: "hei på deg "+ user_fields.first_name}).then(() => {
+
+             sendMessage(senderID, { text: '🎈' });
+           });
+
+  }
+
+     }
+   });
+  }

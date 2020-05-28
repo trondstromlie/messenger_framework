0f2e41196471d3365933e50c975e6b0dd73468f7
Trond Strom <mybestlabs@gmail.com>
2020-05-28 14:20:44 +0200

    lks

--- processes/postback.js
+++ processes/postback.js
@@ -7,7 +7,7 @@ module.exports = function processPostback(event) {
   const payload = event.postback.payload;
 
   if(payload === "alt1") {
-    console.log('alt 1 er registrert i payload'});
+    console.log('alt 1 er registrert i payload');
     sendMessage(senderID, {text:"alternativ 1 er registrert"});
   }
 

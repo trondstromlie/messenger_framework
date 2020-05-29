7093e494cdd8ed6d3c524fba3511b43e79ad3c0f
Trond Strom <mybestlabs@gmail.com>
2020-05-29 14:19:48 +0200

    ny messenger integration

--- messengerApp/webhook.js
+++ messengerApp/webhook.js
@@ -2,10 +2,52 @@ const express = require('express');
 const router = express.Router();
 const config = require('config');
 const User = require("../models/User");
+const request = require('request');
 
-const handleMessage = (psid, message) => {
-  console.log(message.text);
+//construct the message body
+const function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  // Send the HTTP request to the Messenger Platform
+  
+ request({
+   "uri": "https://graph.facebook.com/v2.6/me/messages",
+   "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+   "method": "POST",
+  }, (err, res, body) => {
+   if (!err) {
+    console.log('message sent!')
+   } else {
+     console.error("Unable to send message:" + err);
+   }
+ });
+ }
 };
+
+
+//handle message
+//function
+const  handleMessage (sender_psid, received_message) => {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
 
 const handlePostback = (psid, postback) => {
   console.log(postback);

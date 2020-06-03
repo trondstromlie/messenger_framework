e8284d13178a7309742665af1a8fe8e9fd1529ea
Trond Strom <mybestlabs@gmail.com>
2020-06-03 09:47:02 +0200

    messenger bot

---
+++ messengerApp/controllers/callSendAPI.js
@@ -0,0 +1,27 @@
+//send message function
+//construct the message body
+const request = require("request");
+
+module.exports = function callSendAPI (sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+   }
+   // Send the HTTP request to the Messenger Platform
+
+  request({
+   "uri": "https://graph.facebook.com/v2.6/me/messages",
+   "qs": { "access_token": config.get('FbPageToken') },
+   "method": "POST",
+   "json": request_body
+  }, (err, res, body) => {
+   if (!err) {
+    console.log('message sent!')
+   } else {
+     console.error("Unable to send message:" + err);
+   }
+ });
+ }

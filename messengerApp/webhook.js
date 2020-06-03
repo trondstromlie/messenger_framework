15cdab82831f384a400027a49ced9bc90db2509b
Trond Strom <mybestlabs@gmail.com>
2020-06-03 10:47:57 +0200

    messenger bot

--- messengerApp/webhook.js
+++ messengerApp/webhook.js
@@ -103,6 +103,7 @@ router.post("/", ( req , res ) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
+        console.log(webhook_event.message)
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);

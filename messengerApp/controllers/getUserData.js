6988b182e948d86d59c063f09f231c4ee4748b59
Trond Strom <mybestlabs@gmail.com>
2020-06-03 12:34:58 +0200

    legger inn messenger user data

--- messengerApp/controllers/getUserData.js
+++ messengerApp/controllers/getUserData.js
@@ -5,10 +5,10 @@ const request = require('request');
  module.exports = async function (sender_psid) {
   var userData = {};
 
-  await request({ url: "https://graph.facebook.com/v2.6/" + sender_psid,
+  request({ url: "https://graph.facebook.com/v2.6/" + sender_psid,
             qs: { access_token: config.get("FbPageToken"),
             fields: "first_name,last_name" }, method: "GET"
-            }, (error, response, body) =>
+          }, await (error, response, body) =>
               {
                 if(!error) {
                 console.log(body)

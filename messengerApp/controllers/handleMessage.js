8fd7d907754ba44c7ea62e3df7e2ff391a483025
Trond Strom <mybestlabs@gmail.com>
2020-06-04 08:53:08 +0200

    legger inn messenger user data

--- messengerApp/controllers/handleMessage.js
+++ messengerApp/controllers/handleMessage.js
@@ -11,57 +11,25 @@ module.exports = async function handleMessage (sender_psid, received_message) {
   //console.log(JSON.stringify(received_message));
 
 
+  //check if messege contain the word "hei"
+  try {
+    
+    if(received_message.text === "Hei") {
 
-  // Check if the message contains text
-  if (received_message.text === "Image") {
+      let userdata = await getUserData(sender_psid);
 
-    let response;
-    let userdata = await getUserData(sender_psid);
+      let response = {"text":"hello " +userdata.first_name+ " du skriver med roboten nå"};
 
-    console.log({"user_data" : userdata});
+      await callSendAPI(sender_psid,response);
 
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${userdata.first_name +received_message.text}". Now send me an image!`
+      return Void;
+
     }
-    // Sends the response message
-    return callSendAPI(sender_psid, response);
+
+  } catch(e) {
+    console.error(e.message);
+  }
 
 
-  } else if ( received_message.text === "Start") {
-
-    let response;
-    let userdata = await getUserData(sender_psid);
-    response = {"text": userdata.first_name + "er kul"};
-    console.log({"user_data" : userdata});
-    console.log("start is active");
-
-      // Sends the response message
-      return callSendAPI(sender_psid, response);
-
-  } else if (received_message.text) {
-    try {
-      let response;
-      const userdata = await getUserData(sender_psid);
-
-      console.log({"userdata else" : userdata});
-        //send the responce message
-        if (userdata.first_name === "Trond") {
-
-        //create payload for messages
-        response = {"text": `hei ${userdata.first_name} <3 :) du er flink`};
-        console.log(response);
-
-        return callSendAPI(sender_psid, response);
-
-      } else {
-        response = {"text":"hallo " + userdata.first_name}
-        return callSendAPI(sender_psid, response );
-      }
-    } catch(e) {
-       console.error(e.message);
-     }
-
-}
 
   };

1f9fd5e8485b98a5558407eda4a54c759413033f
Trond Strom <mybestlabs@gmail.com>
2020-06-18 14:35:47 +0200

    legger inn customfields og process for essenger

--- messengerApp/controllers/handleMessage.js
+++ messengerApp/controllers/handleMessage.js
@@ -23,9 +23,12 @@ const logMessage = require("./writeToLog");
 
 module.exports = async function handleMessage (sender_psid, received_message) {
 
-let userdata = await fetchUserData(sender_psid);
 
-if(!received_message.is_echo == true) await logMessage(sender_psid, received_message.text, userdata.user.first_name);
+
+
+if(!received_message.is_echo == true) {
+  let userdata = await fetchUserData(sender_psid);
+  let log = await logMessage(sender_psid, received_message.text, userdata.user.first_name);}
 
 if(received_message.is_echo == true) {
 
@@ -33,86 +36,30 @@ console.log({"is_echo:":received_message.text});
 
 
 }  //check if messege contain the word "Hei"
-else if(received_message.text === "Hei") {
+else if(received_message.text === "Init") {
 
   console.log({"received_message:":received_message.text});
 
   try {
 
+      //add the process get_personel to the user fields
+      //is activ true step 0
 
-
-      let response = {"text":"hello " +userdata['user']['first_name']+ " du skriver med roboten nå"};
+      let response = {"text":"hello " + userdata['user']['first_name'] + " du skriver med roboten nå"};
 
       await callSendAPI(sender_psid , response);
 
 
-
-
       return null;
 
 
 
   } catch(e) {
     console.error(e.message);
-  }
+   }
   //end
  //check if name is trond contain the word "and message contain word init"
-}  else if(received_message.text === "Start") {
-
-  try {
-
-
-
-
-      if (userdata.user.first_name === "Kristina") {
-        let response = {"text":"hei  " + userdata['user']['first_name'] + " Jeg er ekstra nyforelska i deg i dag <3"};
-
-        await callSendAPI(sender_psid , response);
-        return null;
-
-      } else if (userdata.user.first_name === "Trond") {
-         let response = {"text":"hello " +userdata['user']['first_name']+ " du heter ikke kristina vell?"};
-
-
-         await callSendAPI(sender_psid , response);
-
-      return null;
-      }
-
-      else {
-
-
-
-        let response = {"text":"Jeg kjenner ikke deg"};
-
-
-        await callSendAPI(sender_psid , response);
-
-        return 0
-      }
-
-  } catch(e) {
-    console.error(e.message);
-  }
-
+ }
   //end
 
-}  else if(received_message.text) {
-
-  try {
-
-
-
-        let response = {"text":"hei <3 "+ userdata.user.first_name +" du skrev " + received_message.text};
-
-
-        await callSendAPI(sender_psid , response);
-
-
-  } catch(e) {
-    console.error(e.message);
-  };
-  //end
-
-  }
-};
+}

10ff8f5f5194d123942f4c102a4e8c3f63863264
Trond Strom <mybestlabs@gmail.com>
2020-06-19 12:53:11 +0200

    lager userfields integration

--- messengerApp/controllers/handleMessage.js
+++ messengerApp/controllers/handleMessage.js
@@ -73,7 +73,7 @@ try {
 
       let arr = ["trond","kristina","eller","rolf"];
 
-      let index = arr.filter(item => arr.Id);
+      let index = arr.filter(item => arr === "rolf");
       console.log({idex:index})
 
       await callSendAPI(sender_psid, responce);

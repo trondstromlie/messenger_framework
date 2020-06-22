5fcd50cdf9f85f401be8dc77762f2605ec78af2e
Trond Strom <mybestlabs@gmail.com>
2020-06-22 15:40:37 +0200

    leeger inn messenger processes

--- messengerApp/controllers/process_loop.js
+++ messengerApp/controllers/process_loop.js
@@ -19,11 +19,10 @@ const fs = require('fs');
 
 async function user_loop (process_name , user_obj, index , incoming_msg ) {
   console.log("process loop is starting!");
-  console.log(user_obj);
   let user = user_obj;
   let step = user.messenger_processes[index].process_progress;
   let processName = process_name;
-  let sender_psid = user_obj.sender_psid;
+  let sender_psid = user.sender_psid;
 
   //the user processes will be omprtetd from a registerd users db in the future
   //**************************************************************

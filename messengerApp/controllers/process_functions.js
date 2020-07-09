a7e57207221c49b207c91e4a183d15e3a2a021e4
trondstromlie <mybestlabs@gmail.com>
2020-07-09 10:34:30 +0200

    leggger inn sender action

--- messengerApp/controllers/process_functions.js
+++ messengerApp/controllers/process_functions.js
@@ -373,9 +373,9 @@ async function listen_for_data(sender_psid, user, message, custom_field_obj, qui
 async function writing_action (sender_psid, user, message, custom_field_obj, quick_reply_obj, in_message , bool_obj, jump_to ,err_message, messenger_processess) {
 
 
-  await senderAction(sender_psid, "writing_on");
+  await senderAction(sender_psid, "typing_on");
   await sleep(5000);
-  await senderAction(sender_psid, "writing_off");
+  await senderAction(sender_psid, "typing_off");
   return {status:true,step:"next"};
 }
 

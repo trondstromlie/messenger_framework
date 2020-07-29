541760e7e4916f61d59d02430ba1837d79c7f519
trondstromlie <mybestlabs@gmail.com>
2020-07-29 14:19:46 +0200

    creating the getting startetd function

--- messengerApp/controllers/handleReferral.js
+++ messengerApp/controllers/handleReferral.js
@@ -14,6 +14,7 @@ module.exports = async function handleReferral (sender_psid, received_message) {
     user = fetchUserData(sender_psid);
 
     console.log("************ starting handle refferal   ************ ");
+    console.log(recieved_message);
     
   if ( received_message.referral ) {
         console.log("referal discovered");

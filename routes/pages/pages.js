5cf27516a5b0206a49f02f9d9e46e298e28cae9a
trondstromlie <mybestlabs@gmail.com>
2020-08-05 11:11:00 +0200

    lager webview

--- routes/pages/pages.js
+++ routes/pages/pages.js
@@ -11,7 +11,16 @@ router.get("/",function(req,res) {
 
 router.get("/webview",function(req,res) {
 
-  res.render("webview");
+  let query_message = " ";
+
+  if(req.query.message) {
+     query_message = req.query.message
+  }else {
+     query_message = "Hello"
+  }
+  
+
+  res.render("webview", {"title":query_message});
 
 });
 

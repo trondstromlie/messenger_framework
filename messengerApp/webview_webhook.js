05b9144ecc85ac635d9f14bb9f64117f1e188646
trondstromlie <mybestlabs@gmail.com>
2020-08-06 14:59:03 +0200

    lager webview

---
+++ messengerApp/webview_webhook.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require('express');
+
+"use strict";
+const express = require('express');
+const router = express.Router();
+const config = require('config');
+//const User = require("../models/User");
+const handleMessage = require("./controllers/handleMessage");
+const handlePostBack = require('./controllers/handlePostback');
+const handleReferral = require('./controllers/handleReferral');
+
+router.get("/", (req, res) => {
+    res.send("GET @ messengerapp/webview_webhook");
+});
+
+
+
+module.exports = router;

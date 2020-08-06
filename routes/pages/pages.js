a95d692f467ebfb08c0d0bf1e65f3233b7f397a0
trondstromlie <mybestlabs@gmail.com>
2020-08-06 12:20:43 +0200

    lager webview

--- routes/pages/pages.js
+++ routes/pages/pages.js
@@ -30,7 +30,7 @@ router.get("/webview", async function(req,res) {
 
   if(req.query.message)  query_message = req.query.message;
   if(req.query.sender_psid)  sender_psid = req.query.sender_psid;
-  if(req.query.field_name0) field_name = req.query.field_name
+  if(req.query.field_name0) field_name = req.query.field_name;
 
   console.log({sender_psid:sender_psid});
 

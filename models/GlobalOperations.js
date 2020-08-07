7a3d9374adbfd77c55c8cf749d4dca44746b9b86
trondstromlie <mybestlabs@gmail.com>
2020-08-07 10:24:19 +0200

    lager webview

---
+++ models/GlobalOperations.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const GlobalOperations = new mongoose .Schema({
+
+    crontab_loop: [
+        {
+            sender_psid:{
+                type:String,
+                required:true
+            },
+            timestamp:{
+                type:Date,
+                required:true
+            },
+            messenger_process:{
+                type:String,
+                required:true
+            },
+            custom_data_name:{
+                type:String,
+                required:true
+            },
+            custom_data_value:{
+                type:String,
+                required:true
+            }
+
+        }
+    ]
+})

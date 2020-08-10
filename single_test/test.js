ed43603da69d3293275ed8fc09730c6dd179e975
trondstromlie <mybestlabs@gmail.com>
2020-08-10 11:00:34 +0200

    lager crontab_loop

--- single_test/test.js
+++ single_test/test.js
@@ -1,20 +1,11 @@
 "use strict";
-
 let arr = [
-    {name : "trond", age:40},
-    {name : "kristina" , age: 35},
-    {name : "rolf" , age: 68}
+    {value1 : "a", value2 : "b"},
+    {value1 : "a", value2 : "c"},
+    {value1 : "b", value2 : "b"},
+    {value1 : "a", value2 : "b"}
 ];
 
-let first = arr.filter(item => item.name === "kristina");
-console.log({first:first});
+let value = arr.filter(item => !(item.value1 == "a" && item.value2 == "b"));
 
-
-let second = arr.filter(item => item.name !== "kristina" && item.age !== 35);
-console.log({second:second});
-
-
-let third = arr.filter( (item) => {
-    if(item.name !== "trond" && item.age !== 40) return item;
-})
-console.log({third:third})
+console.log(value);

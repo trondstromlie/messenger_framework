5711069b01bd83ed22331c5e74da3f5b75a89307
trondstromlie <mybestlabs@gmail.com>
2020-08-10 10:27:14 +0200

    lager crontab_loop

--- routes/api/add_userCrontab_loop.js
+++ routes/api/add_userCrontab_loop.js
@@ -127,6 +127,7 @@ router.put("/", [
            console.log({sender_psid:sender_psid, field_name:field_name,field_value:field_value});
 
            let clean = cron.crontab_loop.filter( (item ) => {
+               console.log(item);
                if(sender_psid !== item.sender_psid && field_name !== item.custom_data_name && field_value !== item.custom_data_value) {
                    return true;
                }

779921cef6221e77929b16ebf69b9cf8921e8deb
trondstromlie <mybestlabs@gmail.com>
2020-08-12 11:59:20 +0200

    lager crontab_loop

--- models/GlobalOperations.js
+++ models/GlobalOperations.js
@@ -10,23 +10,23 @@ const GlobalOperations = new mongoose.Schema({
         {
             sender_psid:{
                 type:String,
-                required:true
+                
             },
             timestamp:{
                 type:Date,
-                required:true
+                
             },
             messenger_process:{
                 type:String,
-                required:true
+               
             },
             custom_data_name:{
                 type:String,
-                required:true
+                
             },
             custom_data_value:{
                 type:String,
-                required:true
+                
             }
 
         }
